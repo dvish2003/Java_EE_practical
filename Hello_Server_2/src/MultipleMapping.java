e78482fd8653d0403831446541195502b7cb4a40
vishmee <Vishanchathuranga2003@gmail.com>
2024-12-19 08:48:32 +0530

    commit

---
+++ Hello_Server_2/src/MultipleMapping.java
@@ -0,0 +1,22 @@
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+/**
+ * Author: vishmee
+ * Date: 12/19/24
+ * Time: 8:46 AM
+ * Description:
+ */
+@WebServlet
+public class MultipleMapping extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        /*super.doGet(req, resp);*/
+
+        System.out.println("Multiple Mapping is Done");}
+}
